bad8e9a04e293542bb0c6703387f640141ad9fc1
unknown <mariajesuslozada10@gmail.com>
2025-12-19 16:47:43 -0400

    feat: creating dtos to forgot password and reserpassword

--- Entities/Dtos/Auth/LoginResponse.cs
+++ Entities/Dtos/Auth/LoginResponse.cs
@@ -20,4 +20,17 @@
     { 
         public required string RefreshToken {  get; set; }
     }
+
+    public class ForgotPasswordDto
+    {
+        public string Email { get; set; } = string.Empty;
+    }
+
+    public class ResetPasswordDto
+    {
+        public string Token { get; set; } = string.Empty;
+        public string NewPassword { get; set; } = string.Empty;
+    }
+
+
 }
